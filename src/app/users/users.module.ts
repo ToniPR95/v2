0926a2c23c0dc5dca699e7cf53e8419892c470c3
ToniPR95 <toni.podobnik.r@gmail.com>
2021-08-13 09:20:22 +0200

    #5564 navbar created

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,17 +1,14 @@
+import { GridModule } from '@progress/kendo-angular-grid';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 
+import { products } from './products';
 
 @NgModule({
-  declarations: [
-    UsersComponent
-  ],
-  imports: [
-    CommonModule,
-    UsersRoutingModule
-  ]
+  declarations: [UsersComponent],
+  imports: [CommonModule, UsersRoutingModule, GridModule],
 })
-export class UsersModule { }
+export class UsersModule {}
