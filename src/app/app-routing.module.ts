7ba91a318f9291f266fbfc8c08ea5bc1bacb8d3d
ToniPR95 <toni.podobnik.r@gmail.com>
2021-08-18 14:04:55 +0200

    user component inside users module works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,10 +25,6 @@ const routes: Routes = [
       ),
   },
   {
-    path: 'user',
-    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
-  },
-  {
     path: '**',
     redirectTo: 'login',
     pathMatch: 'full',
