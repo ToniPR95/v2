0154c75c9c8176da2dc32ec22570bc81e90074b0
ToniPR95 <toni.podobnik.r@gmail.com>
2021-08-23 11:11:43 +0200

    users.componetn.ts changed

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -11,12 +11,23 @@ import { Observable } from 'rxjs';
 })
 export class UsersComponent implements OnInit {
   public gridData: Observable<any> = new Observable();
+  usersSubscribe: any;
 
   constructor(private users: UsersService) {}
 
   ngOnInit(): void {
     this.gridData = this.users.fetchUsers();
     console.log(this.gridData);
+    this.usersSubscribe = this.gridData.subscribe(
+      (response) => {
+        console.log(response);
+        this.usersSubscribe = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    // console.log(this.usersSubscribe);
   }
 
   isRowInfo(dataItem: object) {
