228a3f7c2466674321d0f71b4f04211b0aa7cfc1
ToniPR95 <toni.podobnik.r@gmail.com>
2021-08-24 10:30:10 +0200

    UserPage completed

--- src/app/users/user-posts/user-posts.component.ts
+++ src/app/users/user-posts/user-posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { UserPostsService } from 'src/app/user-posts.service';
 
 @Component({
@@ -8,14 +9,28 @@ import { UserPostsService } from 'src/app/user-posts.service';
 })
 export class UserPostsComponent implements OnInit {
   userposts: any;
+  public id!: string;
+  userpostsIDs: any;
+  userpostsByID: any;
+  length: any;
 
-  constructor(private userPosts: UserPostsService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private userPosts: UserPostsService
+  ) {}
 
   ngOnInit() {
+    this.userpostsByID = [];
+    this.id = this.route.snapshot.params['id'];
     this.userPosts.getUserPosts().subscribe(
       (response) => {
         console.log(response);
         this.userposts = response;
+        for (var index in this.userposts) {
+          if (this.userposts[index].userId == this.id) {
+            this.length = this.userpostsByID.push(this.userposts[index]);
+          }
+        }
       },
       (error) => {
         console.log(error);
